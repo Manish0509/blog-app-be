0f4425d9885a9baa0a9c970879b2c8290788b44c
Manish5900 <manish.s@brilworks.com>
2025-11-21 23:50:31 +0530

    Updated

--- src/app.js
+++ src/app.js
@@ -4,12 +4,14 @@ import cors from "cors";
 import configRoutes from "./routes/index.js";
 import { connectMongoDB } from "./config/mongoConnect.js";
 
-const app = express();
 dotenv.config();
+const app = express();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 8080; // <-- FIXED
 const mongoURL = process.env.MONGODB_URL;
+
 connectMongoDB(mongoURL);
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
